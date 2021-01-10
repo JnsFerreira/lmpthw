2e3b409cfcecd8f81bda9b14beb8cd0eccee1b0e
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2021-01-10 18:24:46 -0300

    Adding TODO to challange 4

--- challenge_4/command_line_arguments.py
+++ challenge_4/command_line_arguments.py
@@ -1,1 +1,13 @@
-import argparse
+# Steps
+
+# Show help with --help or -h
+# Three or more arguments that are flags, meaning they turn on or off something
+# Three or more arguments that are options, meaning they do take an argument and set a variable 
+# Additional "positional" arguments wich are kust of files.
+
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description = "Challenge 4")
+
+
