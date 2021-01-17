13ca8842c77e714728589f8fea3a8c8aba304feb
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2021-01-17 19:08:04 -0300

    Improving Finder Class with more features

--- challenge_6/find.py
+++ challenge_6/find.py
@@ -11,17 +11,19 @@ from argparse import ArgumentParser
 def main():
     parser = ArgumentParser(description="A pythonic way of linux find command")
 
-    parser.add_argument("dir", metavar="D",
+    parser.add_argument("dir", type=str, nargs=1,
                         help="Directory to search files")
 
-    parser.add_argument("-n", "--name", 
+    parser.add_argument("-n", "--name", type=str,
                         help="Filter files by name")
     
+    parser.add_argument("-t", "--type", type=str,
+                        help="filter files by type")
     
-    parser.add_argument("-p", "--print", action='store_true',
+    parser.add_argument("-s", "--show", action='store_true',
                         help="If used, print all founded files")
     
-    parser.add_argument("-e", "--exec",
+    parser.add_argument("-cmd", "--command", type=str,
                         help="Receive a command and execute on all founded files")
 
     args = parser.parse_args()
--- challenge_6/finder.py
+++ challenge_6/finder.py
@@ -8,28 +8,71 @@
 
 import os
 import glob
+import sys
+from pathlib import Path
 
 class Finder:
     """A Pythonic way of linux find command"""
     
     def __init__(self, args):
-        self.files = list()
         self.args = args
+        self.founded = list()
+        self.start_path = Path(self.args.dir[0])
 
-
-    def __list_directory(self, path):
-        if(os.path.exists(path)):
-            self.files.append(os.listdir(path))
-        
+    def __check_dir(self):
+        if os.path.exists(self.args.dir[0]):
+            pass
         else:
-           print("No such file or directory")
+            print("No such file or directory")
+            sys.exit(1)
 
-    def __filter_files(self, condition):
-        return glob.glob(condition)    
-            
-    def __show_files(self):
-        for f in self.files:
-            print(f)
+    def __find_by_name(self):
+        
+        filtered = self.start_path.rglob(self.args.name)
+        
+        self.founded.append(filtered)
+
+
+    def __find_by_type(self):
+        
+        content = self.start_path.rglob(self.args.name or "*")
+
+        if self.args.type == "d":
+            filtered = [f for f in content if f.is_dir()]
+        
+        elif self.args.type =="f":
+            filtered = [f for f in content if f.is_file()]
+
+        else:
+            print(f"Invalid type {self.args.type}. Supported types: 'd' and 'f'.")
+            sys.exit(1)    
+        
+        self.founded.append(filtered)
+
+    def __execute_cmd(self):
+        for f in self.founded[0]:
+            os.system(self.args.command.format(f))
+
 
     def find(self):
-        print(self.args.dir)
+        self.__check_dir()
+
+        if self.args.name and not self.args.type:
+            self.__find_by_name()
+
+        elif self.args.type:
+            self.__find_by_type()
+
+        else:
+            print("You need either --name or --type")
+            sys.exit(1)
+
+        if self.args.command:
+            self.__execute_cmd()
+
+        if self.args.show:
+            for f in self.founded[0]:
+                print(f)
+            
+    
+
