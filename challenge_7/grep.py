7521c404f50c50e320210271e4fdcae60f5604ca
Jonas Ferreira <jonas.abreuferreira@gmail.com>
2021-01-24 19:36:09 -0300

    Initializing repo

--- challenge_7/grep.py
+++ challenge_7/grep.py
@@ -1,7 +1,28 @@
+from argparse import ArgumentParser
 from grepy import Grep
 
+def parse_args():
+    parser = ArgumentParser(description="Global Regular Expression Print")
+
+    parser.add_argument("pattern", type=str, nargs=1, 
+                        help="A Regex expression")
+
+    parser.add_argument("directory", type=str, nargs=1,
+                        help="Directory to search for patterns into files")
+
+    parser.add_argument("-r", "--recursive", action="store_true",
+                        help="")
+
+    return parser.parse_args()
+
+
 def main():
-    pass
+    
+    args = parse_args()
+
+    grep = Grep()
+
+    grep.search(pattern = args.pattern[0], directory = args.directory[0])
 
 if __name__ == '__main__':
     main()
--- challenge_7/grepy.py
+++ challenge_7/grepy.py
@@ -1,5 +1,29 @@
 import re
+import os
+from pathlib import Path
 
 class Grep:
-    def __init__(self):
-        pass
+
+    def __read_file(self, file_name):
+        try:
+            lines = open(file_name).readlines()
+        except UnicodeDecodeError:
+            print(f"Binary file {file_name} matches.")
+            
+            return lines
+
+    def __find_in_file(self, lines, pattern):
+        exp = re.compile(pattern)
+
+        for line in lines:
+            if exp.search(line):
+                print(line)
+
+    def search(self, pattern, directory):
+        start_path = Path(directory)
+
+        for f in start_path.rglob("*"):
+            if f.is_file():
+                lines = self.__read_file(file_name = f)
+
+                self.__find_in_file(lines=lines, pattern=pattern)
